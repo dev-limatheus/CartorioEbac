ee862dd38a8d8d94ab6324f3ec367be2665e4b5e
Matheus Lima <71335405+dev-limatheus@users.noreply.github.com>
2023-11-08 10:32:35 -0300

    Corrigindo o Login

--- Cartorio.c
+++ Cartorio.c
@@ -114,15 +114,17 @@ int main()
  	int opcao=0; //definindo variavel
  	int laco=1; 
  	char senhadigitada[10]="a";
+ 	int comparacao;
  	
  	printf("### Cartório da EBAC ###\n\n");
  	printf("Login de administrador!\n\nDigite a sua senha: ");
  	scanf("%s",senhadigitada);
  	
- 	if(senhadigitada[10] == "admin")
+ 	comparacao = strcmp(senhadigitada, "admin");
+ 		
+ 	if(comparacao == 0)
  	{
-	 
- 	
+ 		system("cls");
  		for(laco=1;laco=1;)
  		{
 		 	system("cls");
